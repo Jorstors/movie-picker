2713a81886204b4de68d36bd64685f9ce7a897d0
Justus <justus1274@gmail.com>
2025-10-01 02:30:31 -0700

    Schema syntax fix

--- backend/SQL_UTIL/SCHEMA.sql
+++ backend/SQL_UTIL/SCHEMA.sql
@@ -4,14 +4,14 @@ CREATE TABLE IF NOT EXISTS events (
   date TIMESTAMP NOT NULL,
   time VARCHAR(50) NOT NULL,
   location VARCHAR(255) NOT NULL,
-  author VARChar(255) NOT NULL
+  author VARCHAR(255) NOT NULL
 )
 
 CREATE TABLE IF NOT EXISTS rsvps (
   id BIGSERIAL PRIMARY KEY,
-  event_id BITINT NOT NULL,
+  event_id BIGINT NOT NULL,
   FOREIGN KEY (event_id) REFERENCES events(id) ON DELETE CASCADE,
   author VARCHAR(255) NOT NULL,
-  movie VARCHAR(255) NOT NULL
+  movie VARCHAR(255) NOT NULL,
   UNIQUE (event_id, author)
 )
