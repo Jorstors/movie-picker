a6d9f748ebfec8311b569f950dcfd720726cf328
Justus <justus1274@gmail.com>
2025-10-06 01:55:01 -0700

    Added user context and an on load username prompt

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,25 @@ import EventFetch from "@/lib/Events";
 import TabBar from "@/components/TabBar";
 import AddEdit from "@/components/AddEditEvents";
 import EventDialogue from "@/components/EventDialogue";
-import { useEffect, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import type { Event } from "@/lib/types";
+import PromptName from "@/components/PromptName";
+
+type userContextType = {
+  user: string;
+  setUser: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const userContext = createContext<userContextType>({} as userContextType);
 
 function Home() {
   const [events, setEvents] = useState<Event[]>([]);
   const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+    console.log("User context updated:", user);
+  }, [user]);
 
   // Fetch events from the database
   useEffect(() => {
@@ -32,42 +45,48 @@ function Home() {
   }, []);
 
   return (
-    <main className="min-h-screen w-full grid place-items-center bg-gradient-to-b from-background to-card" >
-      <div className="px-5 min-w-xs md:w-3xl lg:w-5xl min-h-screen flex flex-col gap-10 items-center content-start py-20">
-        {/* Tab Bar */}
-        <div>
-          <TabBar />
-        </div>
+    <userContext.Provider value={{ user, setUser }}>
+      <main className="min-h-screen w-full grid place-items-center bg-gradient-to-b from-background to-card" >
+        <div className="px-5 min-w-xs md:w-3xl lg:w-5xl min-h-screen flex flex-col gap-10 items-center content-start py-20">
+          {/* User Name Context */}
+          <PromptName />
 
-        {/* Add & Edit */}
-        <div>
-          <AddEdit />
-        </div>
+          {/* Tab Bar */}
+          <div>
+            <TabBar />
+          </div>
 
-        {/* Event Cards */}
-        {loading ? (
-          <p>Loading events...</p>
-        ) : (
-          events.length === 0 ? (
-            <p>No events found.</p>
+          {/* Add & Edit */}
+          <div>
+            <AddEdit />
+          </div>
+
+          {/* Event Cards */}
+          {loading ? (
+            <p>Loading events...</p>
           ) : (
-            events.map((event: Event) => (
-              <EventDialogue
-                key={event.id}
-                id={event.id}
-                title={event.title}
-                genre={event.genre}
-                date={event.date}
-                time={event.time}
-                location={event.location}
-                author={event.author}
-              />
-            ))
-          ))}
-      </div>
-    </main>
+            events.length === 0 ? (
+              <p>No events found.</p>
+            ) : (
+              events.map((event: Event) => (
+                <EventDialogue
+                  key={event.id}
+                  id={event.id}
+                  title={event.title}
+                  genre={event.genre}
+                  date={event.date}
+                  time={event.time}
+                  location={event.location}
+                  author={event.author}
+                />
+              ))
+            ))}
+        </div>
+      </main>
+    </userContext.Provider>
   );
 }
 
 export default Home;
 export type { Event };
+export { userContext };
---
+++ components/PromptName.tsx
@@ -0,0 +1,56 @@
+import { userContext } from "@/app/page"
+import { useContext, useState } from "react"
+import { Input } from "./ui/input";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button } from "./ui/button";
+
+export default function PromptName() {
+  const { user, setUser } = useContext(userContext);
+  const [name, setName] = useState<string>("");
+
+  // Card that dissappears once name is submitted
+  return (
+    user !== "" ? <></> : (
+      <>
+        <Card className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] z-50">
+          <CardHeader>
+            <CardTitle className="text-2xl">Welcome!</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Input
+              type="text"
+              placeholder="Enter your name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              autoFocus
+              onKeyDown={(e) => {
+                if (e.key === 'Enter' && name.trim() !== "") {
+                  setUser(name.trim());
+                }
+              }}
+            />
+            <Button
+              className="mt-4 w-full hover:cursor-pointer"
+              onClick={() => {
+                if (name.trim() !== "") {
+                  setUser(name.trim());
+                }
+              }}
+            >
+              Submit
+            </Button>
+            <CardDescription className="my-4">Please enter your name to get started.</CardDescription>
+
+          </CardContent>
+        </Card >
+        <div className="fixed inset-0 bg-black opacity-50 z-40"></div>
+      </>
+    )
+  );
+};
